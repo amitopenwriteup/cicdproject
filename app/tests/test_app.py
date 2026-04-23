6df5075260cb83833c4c41847c048ee71666c4f7
amit <amit@ow.com>
2026-04-23 15:18:32 +0000

    Add

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add project root to Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from app import app
 
 def test_index():
